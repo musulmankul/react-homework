4359109de0d3c1c30663ba8af6b159718cf23f73
musulmankul <sultanovmusulmankul@gmail.com>
2024-11-19 12:57:45 +0600

    loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
+import { useEffect, useState } from "react";
 import {ProductCard} from "./components/ProductCard";
 
-const products = [
-    {
-        id: 0,
-        name: "Product 1",
-        category: "Category 1",
-        price: 200,
-        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhadelMSJi32_CDXbYMPiUYGl9nuQNYCQdJg&s"
-
-    },
-    {
-        id: 1,
-        name: "Product 2",
-        category: "Category 1",
-        price: 100,
-        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Daewoo_Nexia_front_20071228.jpg/1200px-Daewoo_Nexia_front_20071228.jpg"
-
-    },
-]
-
 function App() {
+    const [loading, setLoading] = useState(true);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setProducts(json))
+            .finally(()=>setLoading(false));
+    }, []);
+
     return (
         <div>
+            {loading && <div>Loading...</div>}
             {products.map((product) => (
               <ProductCard data={product} />
             ))}
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,14 +1,14 @@
 import "./index.css"
 
 export function ProductCard(props) {
-    const { img, category, name, price} = props.data;
+    const { image, category, title, price} = props.data;
   
     return (
       <div className="product-card">
-            <img width="200" src={img} alt='' />
-            <div>{category}</div>
-            <h4>{name}</h4>
-            <div>{price}</div>
+          <img width="200" src={image} alt='' />
+          <div>{category}</div>
+          <h4>{title}</h4>
+          <div>{price}</div>
       </div>
     )
 };
